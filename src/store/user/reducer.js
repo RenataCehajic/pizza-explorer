cc313b9ac72fe06980296e1695408bf31e455ee5
RenataCehajic <renata.cehajic@gmail.com>
2021-03-15 15:31:33 +0100

    Initial setup with hardcoded data

---
+++ src/store/user/reducer.js
@@ -0,0 +1,13 @@
+// src/store/user/reducer.js
+const initialState = {
+  name: "Renata",
+  id: 40,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}

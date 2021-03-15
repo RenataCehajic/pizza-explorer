b20333c9e3f9954325a0de14c87591ac151615e1
RenataCehajic <renata.cehajic@gmail.com>
2021-03-15 18:36:13 +0100

    Add restaurants slice and RestaurantList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import PizzaList from "./components/PizzaList";
+import RestaurantList from "./components/RestaurantList";
+import AddPizzaForm from "./components/AddPizzaForm";
 
 function App() {
   return (
     <div className="App">
       <PizzaList />
+      <AddPizzaForm />
+      <RestaurantList />
     </div>
   );
 }
-
 export default App;

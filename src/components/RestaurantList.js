d4ce2f95f1904c6c1715b9009ddb1f16a305bcf5
RenataCehajic <renata.cehajic@gmail.com>
2021-03-16 11:35:56 +0100

    Add "Who sells pizza" dropdown

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,13 +1,34 @@
 // src/components/PizzaList.js
 import { useSelector } from "react-redux";
 import { selectRestaurantsWithPizzas } from "../store/selectors";
+import { selectPizzas } from "../store/pizzas/selectors";
+import { useState } from "react";
 
 export default function RestaurantList() {
   const restaurants = useSelector(selectRestaurantsWithPizzas);
+  const pizzas = useSelector(selectPizzas);
+
+  const [selectedPizza, setSelectedPizza] = useState(pizzas[0].id);
 
   return (
     <div>
       <h1>Restaurants</h1>
+      <h2>
+        Who sells
+        <select
+          value={selectedPizza}
+          onChange={(e) => {
+            setSelectedPizza(parseInt(e.target.value));
+          }}
+        >
+          {pizzas.map((pizza) => (
+            <option key={pizza.id} value={pizza.id}>
+              {pizza.name}
+            </option>
+          ))}
+        </select>
+        ?
+      </h2>
       <ul>
         {restaurants.map((restaurant) => (
           <li key={restaurant.id}>

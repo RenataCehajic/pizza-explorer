1606e0d160a6340237f525a2215a1827ae9b8f3c
RenataCehajic <renata.cehajic@gmail.com>
2021-03-16 12:15:26 +0100

    Add "What does restaurant sell" dropdown

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,18 +1,58 @@
 // src/components/PizzaList.js
 import { useSelector } from "react-redux";
-import { selectRestaurantsWithPizzas } from "../store/selectors";
+// import { selectRestaurantsWithPizzas } from "../store/selectors";
 import { selectPizzas } from "../store/pizzas/selectors";
 import { useState } from "react";
+// import { selectRestaurantsThatSellPizza } from "../store/restaurants/selectors";
+import { selectPizzasSoldByRestaurant } from "../store/selectors";
+import {
+  selectRestaurants,
+  selectRestaurantsThatSellPizza,
+} from "../store/restaurants/selectors";
 
 export default function RestaurantList() {
-  const restaurants = useSelector(selectRestaurantsWithPizzas);
+  //   const restaurants = useSelector(selectRestaurantsWithPizzas);
+  const restaurants = useSelector(selectRestaurants);
   const pizzas = useSelector(selectPizzas);
 
   const [selectedPizza, setSelectedPizza] = useState(pizzas[0].id);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(
+    restaurants[0].id
+  );
+
+  const restaurantsThatSellSelectedPizza = useSelector(
+    selectRestaurantsThatSellPizza(selectedPizza)
+  );
+
+  const pizzasSoldBySelectedRestaurant = useSelector(
+    selectPizzasSoldByRestaurant(selectedRestaurant)
+  );
 
   return (
     <div>
       <h1>Restaurants</h1>
+      <h2>
+        What does{" "}
+        <select
+          value={selectedRestaurant}
+          onChange={(e) => {
+            setSelectedRestaurant(parseInt(e.target.value));
+          }}
+        >
+          {restaurants.map((restaurant) => (
+            <option key={restaurant.id} value={restaurant.id}>
+              {restaurant.name}
+            </option>
+          ))}
+        </select>{" "}
+        sell?
+      </h2>
+      <ul>
+        {pizzasSoldBySelectedRestaurant.map((pizza) => (
+          <li key={pizza.id}>{pizza.name}</li>
+        ))}
+      </ul>
+
       <h2>
         Who sells
         <select
@@ -30,17 +70,28 @@ export default function RestaurantList() {
         ?
       </h2>
       <ul>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.id}>
-            {restaurant.name}
-            <ul>
-              {restaurant.pizzas.map((pizza) => (
-                <li key={pizza.id}>{pizza.name}</li>
-              ))}
-            </ul>
-          </li>
+        {restaurantsThatSellSelectedPizza.map((restaurant) => (
+          <li key={restaurant.id}>{restaurant.name}</li>
         ))}
       </ul>
     </div>
   );
 }
+
+//   <ul>
+//     {pizzasSoldBySelectedRestaurant.map((pizza) => (
+//       <li key={pizza.id}>{pizza.name}</li>
+//     ))}
+//   </ul>
+//   <ul>
+//     {restaurants.map((restaurant) => (
+//       <li key={restaurant.id}>
+//         {restaurant.name}
+//         <ul>
+//           {restaurant.pizzas.map((pizza) => (
+//             <li key={pizza.id}>{pizza.name}</li>
+//           ))}
+//         </ul>
+//       </li>
+//     ))}
+//   </ul>
